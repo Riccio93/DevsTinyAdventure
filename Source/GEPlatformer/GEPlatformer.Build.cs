6247bb91abf42d28b2e335b95a5831f497846f98
Andrea Ricci <andrearicci500@gmail.com>
2022-05-01 10:34:08 +0200

    Empty Unreal project commit

---
+++ Source/GEPlatformer/GEPlatformer.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GEPlatformer : ModuleRules
+{
+	public GEPlatformer(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
