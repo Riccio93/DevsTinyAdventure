c75316830084ec88efe14be358b8e1913db4445c
Andrea Ricci <andrearicci500@gmail.com>
2022-05-27 09:04:32 +0200

    Implement minor fixes to rat enemy

---
+++ Source/GEPlatformer/RatEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyCharacter.h"
+#include "RatEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEPLATFORMER_API ARatEnemy : public AEnemyCharacter
+{
+	GENERATED_BODY()
+	
+};
